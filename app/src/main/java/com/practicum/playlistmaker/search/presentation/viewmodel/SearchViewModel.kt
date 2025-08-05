f8a280ce2133592a8ce1a34c7116c12fb2ee6ad8
SemyonKardashin <kardashin.s.n@gmail.com>
2025-08-05 22:33:37 +0500

    Спринт 16, в процессе-2

--- app/src/main/java/com/practicum/playlistmaker/search/presentation/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/practicum/playlistmaker/search/presentation/viewmodel/SearchViewModel.kt
@@ -3,25 +3,49 @@ package com.practicum.playlistmaker.search.presentation.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.practicum.playlistmaker.creator.Creator
 import com.practicum.playlistmaker.search.domain.api.TracksSearchHistoryInteractor
 import com.practicum.playlistmaker.search.domain.api.TracksSearchInteractor
 import com.practicum.playlistmaker.search.domain.model.Track
 
-class SearchViewModel(
-    val searchHistoryInteractor: TracksSearchHistoryInteractor
-) : ViewModel() {
-    private val _tracks = MutableLiveData<List<Track>>(emptyList())
-    val tracks: LiveData<List<Track>> = _tracks
-
-    private val _savedText = MutableLiveData<String>("")
-    val savedText: LiveData<String> = _savedText
-
-    private val searchIneractor: TracksSearchInteractor by lazy {
-        Creator.provideTracksSearchInteractor()
+data class SearchScreenState(
+    val tracks: List<Track> = emptyList<Track>(),
+    val savedText: String = "",
+    val screenStatus: ScreenStatus = ScreenStatus.Default,
+) {
+    sealed class ScreenStatus {
+        object Default : ScreenStatus()
+        object Loading : ScreenStatus()
+        object LoadSuccess : ScreenStatus()
+        object notFoundError : ScreenStatus()
+        object connectionError : ScreenStatus()
     }
-
-
-
-
 }
+
+    class SearchViewModel(
+        val searchHistoryInteractor: TracksSearchHistoryInteractor,
+    ) : ViewModel() {
+        private val _screenState = MutableLiveData<SearchScreenState>(SearchScreenState())
+        val screenState: LiveData<SearchScreenState> = _screenState
+
+        private val searchIneractor: TracksSearchInteractor by lazy {
+            Creator.provideTracksSearchInteractor()
+        }
+
+        fun searchTrack(query: String) {
+            _screenState.postValue(SearchScreenState(emptyList(), query, SearchScreenState.ScreenStatus.Loading))
+            searchIneractor.searchTracks(query)
+        }
+
+        companion object {
+            fun getFactory(searchHistoryInteractor: TracksSearchHistoryInteractor): ViewModelProvider.Factory =
+                viewModelFactory {
+                    initializer {
+                        SearchViewModel(searchHistoryInteractor)
+                    }
+                }
+        }
+    }
