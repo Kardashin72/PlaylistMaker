ee15af177897a22c928070c3e090f87249c01460
kardashin <kardashin.s.n@gmail.com>
2025-08-09 17:30:45 +0500

    Спринт 17. Реализация экрана Медиатеки через фрагменты, в процессе.

--- app/src/main/java/com/practicum/playlistmaker/medialibrary/presentation/ui/MediaLibraryActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/medialibrary/presentation/ui/MediaLibraryActivity.kt
@@ -2,12 +2,30 @@ package com.practicum.playlistmaker.medialibrary.presentation.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayoutMediator
 import com.practicum.playlistmaker.R
+import com.practicum.playlistmaker.databinding.ActivityMediaLibraryBinding
 
 class MediaLibraryActivity : AppCompatActivity() {
+    private var _binding: ActivityMediaLibraryBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_media_library)
+        _binding = ActivityMediaLibraryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.toolbarMediaLibrary.setNavigationOnClickListener { finish() }
+
+        binding.viewPagerMediaLibrary.adapter = MediaLibraryPagerAdapter(this)
+        binding.viewPagerMediaLibrary.offscreenPageLimit = 2
+
+        TabLayoutMediator(binding.tabLayoutMediaLibrary, binding.viewPagerMediaLibrary) { tab, position ->
+            tab.text = when(position) {
+                0 -> "Избранные треки"
+                1 -> "Плейлисты"
+                else -> ""
+            }
+        }.attach()
     }
 }
---
+++ app/src/main/java/com/practicum/playlistmaker/medialibrary/presentation/ui/MediaLibraryPagerAdapter.kt
@@ -0,0 +1,20 @@
+package com.practicum.playlistmaker.medialibrary.presentation.ui
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class MediaLibraryPagerAdapter(
+    activity: FragmentActivity,
+) : FragmentStateAdapter(activity) {
+
+    override fun getItemCount(): Int = 2
+
+    override fun createFragment(position: Int): Fragment = when (position) {
+        0 -> MediaLibraryFragment()
+        1 -> PlaylistsFragment()
+        else -> throw IllegalArgumentException("Invalid position: $position")
+    }
+}
+
+
